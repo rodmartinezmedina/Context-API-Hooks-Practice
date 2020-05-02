4c94ed03c64958eb5d4312fb83a323f245509fff
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-02 16:05:21 +0200

    reading list mini app

---
+++ booklist-app/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const Navbar = () => {
+
+  const { books } = useContext(BookContext);
+  return (
+    <div className='navbar'>
+      <h1>Reading List</h1>
+      <p>Currently you have { books.length } to read... </p>
+    </div>
+  )
+}
+
+
+
+export default Navbar;
